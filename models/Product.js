1465a5a1b6fb5e6c82fd6ee40da1b50a61e89e19
Shall9 <hallshane856@gmail.com>
2018-12-10 10:41:19 -0500

    somthing

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
     const Product = sequelize.define("Product", {
         product_name: DataTypes.STRING,
-        department_name: DataTypes.STRING,
+        description: DataTypes.STRING,
         price:DataTypes.DECIMAL(10, 2),
         stock_quantity:DataTypes.INTEGER,
-        createdAt:DataTypes.DATE,
-        updatedAt:DataTypes.DATE
     });
     return Product;
   };
